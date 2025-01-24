56f5dfd7ecdeacb2fb052f36d3371ffb194b2b60
Mateusz Czajkowski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.czajkowsk2@samsung.com>
2025-01-24 11:09:19 +0100

    init project

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RestTest {
+
+    @Test
+    public void testRestAssured() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getTime());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getHeader("content-type"));
+
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+
+
+}
